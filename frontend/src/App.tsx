ad1ccbe2b9a96beb7eed8d63d751cb7b5789d251
Ali Ahsanuddin <aliahsanuddin@outlook.com>
2025-06-24 12:40:08 -0400

    card list component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { Suspense, lazy } from 'react'
 import PkmnCard from './components/pkmn-card'
+import CardList from './components/card-list'
 
 
 
@@ -7,17 +8,7 @@ import PkmnCard from './components/pkmn-card'
 function App() {
   return (
     <>
-      <p>
-        <PkmnCard
-          imageLowres="https://images.pokemontcg.io/xy1/1_hires.png"
-          imageHires="https://images.pokemontcg.io/xy1/1_hires.png"
-          name="Venusaur-EX"
-          set="XY"
-          price={4.17}
-          percentChange={1.3}
-          volume={100}
-        />
-      </p>
+      <CardList/>
     </>
   )
 }
--- frontend/src/components/card-list.tsx
+++ frontend/src/components/card-list.tsx
@@ -1,2 +1,43 @@
 // list of card-details
 // list rendering with map(), responsive grids, lifting state
+import PkmnCard, { PkmnCardProps } from "./pkmn-card";
+
+export default function CardList () {
+    const cardData: PkmnCardProps[] = [
+        {
+            imageLowres: 'https://images.pokemontcg.io/xy1/1_hires.png',
+            imageHires: 'https://images.pokemontcg.io/xy1/1_hires.png',
+            name: 'Venusaur-EX',
+            set: 'XY',
+            price: 4.17,
+            percentChange: 1.3,
+            volume: 100,
+        },
+        {
+            imageLowres: 'https://images.pokemontcg.io/xy1/1_hires.png',
+            imageHires: 'https://images.pokemontcg.io/xy1/1_hires.png',
+            name: 'Blastoise',
+            set: 'Base Set',
+            price: 10.5,
+            percentChange: -2.1,
+            volume: 85,
+        }
+        // ...more cards
+    ];
+
+    return (
+        <div className="flex flex-row">
+            {cardData.map((card) => 
+                <PkmnCard key={card.price}
+                    imageLowres={card.imageLowres}
+                    imageHires={card.imageHires}
+                    name={card.name}
+                    set={card.set}
+                    price={card.price}
+                    percentChange={card.percentChange}
+                    volume={card.volume}
+                />
+            )}
+        </div>
+    )
+}
